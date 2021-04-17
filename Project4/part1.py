4a952ca4eb7666f97695f3ad8a509f3922504b35
Rocco <febbo87@gmail.com>
2021-04-16 20:19:13 -0400

    part1

---
+++ Project4/part1.py
@@ -0,0 +1,18 @@
+def create_sequences(window_size, stride):
+    lines = ''
+    with open('beatles.txt') as f:
+        for line in f:
+            lines += line
+
+    lines = lines.replace('\n', '=')
+
+    sequences = []
+    stride = 3
+    window_size = 5
+    for c in range(0,len(lines),stride):
+        if(c + window_size < len(lines)):
+            sequences.append(lines[c:c+window_size+1:1])
+
+    with open(f'sequences{stride}_{window_size}','w') as f:
+        for line in sequences:
+            f.write(line + '\n')
