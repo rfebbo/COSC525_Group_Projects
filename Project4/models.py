51e01aa37819cc2572bf78981db22d4f727d394e
Rocco <febbo87@gmail.com>
2021-04-16 22:16:29 -0400

    part4

--- Project4/models.py
+++ Project4/models.py
@@ -1,6 +1,7 @@
 
 import os
 # os.environ['CUDA_VISIBLE_DEVICES']=""
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '0'
 
 import tensorflow as tf
 from tensorflow.keras.models import Sequential
@@ -11,42 +12,40 @@ from tensorflow.keras.layers import LSTM, SimpleRNN
 physical_devices = tf.config.experimental.list_physical_devices('GPU')
 print('physical_devices', physical_devices)
 
-unique_chars=50
-
-def simple_model1():
+def simple_model1(uni):
     model=Sequential()
-    model.add(SimpleRNN(100,input_dim=50))
-    model.add(Dense(50))
+    model.add(SimpleRNN(100,input_dim=uni))
+    model.add(Dense(uni,activation='softmax'))
     return model
 
-def simple_model2():
+def simple_model2(uni):
     model=Sequential()
-    model.add(SimpleRNN(200,input_dim=unique_chars))
-    model.add(Dense(unique_chars))
+    model.add(SimpleRNN(200,input_dim=uni))
+    model.add(Dense(uni,activation='softmax'))
     return model
 
-def simple_model3():
+def simple_model3(uni):
     model=Sequential()
-    model.add(SimpleRNN(100,input_dim=unique_chars, return_sequences=True))
+    model.add(SimpleRNN(100,input_dim=uni, return_sequences=True))
     model.add(SimpleRNN(100, activation='relu'))
-    model.add(Dense(unique_chars))
+    model.add(Dense(uni,activation='softmax'))
     return model
 
-def LSTM_model1():
+def LSTM_model1(uni):
     model=Sequential()
-    model.add(LSTM(100,input_dim=unique_chars))
-    model.add(Dense(unique_chars))
+    model.add(LSTM(100,input_dim=uni))
+    model.add(Dense(uni,activation='softmax'))
     return model
 
-def LSTM_model2():
+def LSTM_model2(uni):
     model=Sequential()
-    model.add(LSTM(200,input_dim=unique_chars))
-    model.add(Dense(unique_chars))
+    model.add(LSTM(200,input_dim=uni))
+    model.add(Dense(uni,activation='softmax'))
     return model
 
-def LSTM_model3():
+def LSTM_model3(uni):
     model=Sequential()
-    model.add(LSTM(100,input_dim=unique_chars, return_sequences=True))
+    model.add(LSTM(100,input_dim=uni, return_sequences=True))
     model.add(LSTM(100, activation='relu'))
-    model.add(Dense(unique_chars))
+    model.add(Dense(uni,activation='softmax'))
     return model
