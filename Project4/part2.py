bb35754912a40fd0c463192e7bafa96b7e2c77e0
AndFly3 <60009017+AndFly3@users.noreply.github.com>
2021-04-17 17:24:57 -0400

    Add files via upload

---
+++ Project4/part2.py
@@ -0,0 +1,34 @@
+from numpy import zeros
+from part1 import create_sequences
+
+
+def createInputOutput(uni, sequences):
+    m = len(sequences)
+    n = len(sequences[0]) - 1
+    p = len(uni)
+    input = zeros((m,n,p))
+    
+    for s, seq in enumerate(sequences):
+        for c, char in enumerate(seq[0:-1]):
+            for u, un in enumerate(uni):
+                if char == un:
+                    input[s][c][u] = 1
+    
+    output = zeros((m,p))
+    for s, seq in enumerate(sequences[0:-1][-1]):
+        for u, un in enumerate(uni):
+            if(sequences[s][-1]) == un:
+                output[s][u] = 1
+    
+    # for s, seq in enumerate(sequences[0:-1]):
+    #     for u, un in enumerate(uni):
+    #         if(sequences[s+1][0]) == un:
+    #             output[s][u] = 1
+    # for u, un in enumerate(uni):
+    #     if(sequences[0][0]) == un:
+    #         output[-1][u] = 1
+    return input, output
+
+
+# u, s = create_sequences(5,3)
+# i, o = createInputOutput(u, s)
